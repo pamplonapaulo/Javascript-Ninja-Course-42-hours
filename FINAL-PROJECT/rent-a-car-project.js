8eae813403792c434df8fc7792386c12ce37cbe2
pamplonapaulo <pamplonapaulo@gmail.com>
2018-07-21 18:28:55 -0300

    melhorando

--- FINAL-PROJECT/rent-a-car-project.js
+++ FINAL-PROJECT/rent-a-car-project.js
@@ -1,4 +1,4 @@
-(function(doc, DOM) {
+(function (doc, DOM) {
   'use strict';
 
     function app(){
@@ -11,7 +11,6 @@
         var $placa = new DOM('[data-js="input-placa"]');
         var $cor = new DOM('[data-js="input-cor"]');
         var $imageUrl = new DOM('[data-js="input-url"]');
-        var $submit = new DOM('[type="submit"]');
         var $message = new DOM('[data-js="message-display"]');
         var $cadastro = new DOM('[data-js="cadastro"]');
         
@@ -77,7 +76,7 @@
             
             newCard.classList.add(newCar.status);
                 
-            var html = '<div array-count="' + carIndex + '" class="my_card"><div class="card__wrapper"><div class="my_front"><div data-js="flip-btn"><i class="material-icons">import_export</i></div><h6>Modelo:</h6><h6 data-js="modelo">' + newCar.modelo + '</h6><h6>Marca:</h6><h6 data-js="marca">' + newCar.marca + '</h6><h6>Ano:</h6><h6 data-js="ano">' + newCar.ano + '</h6><h6>Placa:</h6><h6 data-js="placa">' + newCar.placa + '</h6><h6>Serial Number:</h6><h6 data-js="serial">' + newCar.serial +'</h6><div class="circle" data-js="img" style="background-image: url(\'' + newCar.imageUrl + '\');"></div><h6><i class="material-icons">' + setStatusIcon(carIndex) + '</i></h6></div>' + '<div class="my_back"><div data-js="flip-btn"><i class="material-icons">import_export</i></div><h6 class="status-text">' + setStatusTitle(carIndex) + '</h6><div class="back-card-background"><input data-js="status-switcher" type="range" min="0" max="2" value="' + setLittleCarPosition(carIndex) + '" /><div class="store-wrapper"><span class="store">loja</span><div class="sunbrust"></div><div class="wrap-car-stand"><div class="car-stand"></div></div></div><div class="mechanic-wrapper" ><div class="mechanic"></div></div></div><h6 class="status-icon"><i class="material-icons">' + setStatusIcon(carIndex) + '</i></h6></div></div></div>';
+            var html = '<div array-count="' + carIndex + '" class="my_card"><div class="card__wrapper"><div class="my_front"><div data-js="flip-btn"><i class="material-icons">import_export</i></div><h6>Modelo:</h6><h6 data-js="modelo">' + newCar.modelo + '</h6><h6>Marca:</h6><h6 data-js="marca">' + newCar.marca + '</h6><h6>Ano:</h6><h6 data-js="ano">' + newCar.ano + '</h6><h6>Placa:</h6><h6 data-js="placa">' + newCar.placa + '</h6><h6>Serial Number:</h6><h6 data-js="serial">' + newCar.serial +'</h6><div class="circle" data-js="img" style="background-image: url(\'' + newCar.imageUrl + '\');"></div><h6><i class="material-icons car-status-icon">' + setStatusIcon(carIndex) + '</i></h6></div>' + '<div class="my_back"><div data-js="flip-btn"><i class="material-icons">import_export</i></div><h6 class="status-text">' + setStatusTitle(carIndex) + '</h6><div class="back-card-background"><input data-js="status-switcher" type="range" min="0" max="2" value="' + setLittleCarPosition(carIndex) + '" /><div class="store-wrapper"><span class="store">loja</span><div class="sunbrust"></div><div class="wrap-car-stand"><div class="car-stand"></div></div></div><div class="mechanic-wrapper" ><div class="mechanic"></div></div></div><h6 class="status-icon"><i class="material-icons car-status-icon">' + setStatusIcon(carIndex) + '</i></h6></div></div></div>';
             
             newCard.innerHTML = html;
             
@@ -133,30 +132,32 @@
             if (switcherPosition == 2){
                 carsArray[carIndex].status = 'isFixing';
             }
-            
-            updateHTMLcard(carIndex);
-            setSwitcher();
-            setFlipButtons(carIndex);
+            updateCardStatus(carIndex);
         }
         
-        function updateHTMLcard(index){
+        function updateCardStatus(index){
             
-            var cardHtml = '<li class="' + carsArray[index].status + '"><div array-count="' + index + '" class="my_card is-switched"><div class="card__wrapper"><div class="my_front"><div data-js="flip-btn"><i class="material-icons">import_export</i></div><h6>Modelo:</h6><h6 data-js="modelo">' + carsArray[index].modelo + '</h6><h6>Marca:</h6><h6 data-js="marca">' + carsArray[index].marca + '</h6><h6>Ano:</h6><h6 data-js="ano">' + carsArray[index].ano + '</h6><h6>Placa:</h6><h6 data-js="placa">' + carsArray[index].placa + '</h6><h6>Serial Number:</h6><h6 data-js="serial">' + carsArray[index].serial +'</h6><div class="circle" data-js="img" style="background-image: url(\'' + carsArray[index].imageUrl + '\');"></div><h6><i class="material-icons">' + setStatusIcon(index) + '</i></h6></div>' + '<div class="my_back"><div data-js="flip-btn"><i class="material-icons">import_export</i></div><h6 class="status-text">' + setStatusTitle(index) + '</h6><div class="back-card-background"><input data-js="status-switcher" type="range" min="0" max="2" value="' + setLittleCarPosition(index) + '" /><div class="store-wrapper"><span class="store">loja</span><div class="sunbrust"></div><div class="wrap-car-stand"><div class="car-stand"></div></div></div><div class="mechanic-wrapper" ><div class="mechanic"></div></div></div><h6 class="status-icon"><i class="material-icons">' + setStatusIcon(index) + '</i></h6></div></div></div></li>';
+            var $cardToChange = document.querySelector('[array-count="' + index + '"]').parentElement;
+            var $statusIcon = $cardToChange.querySelectorAll('.car-status-icon');
+            var $statusText = $cardToChange.querySelector('.status-text');
+            var $switcherValue = $cardToChange.querySelector('[data-js="status-switcher"]');
             
-            var $cardToChange = document.querySelector('[array-count="' + index + '"]');
+            $cardToChange.classList.remove('isAvailable', 'isFixing', 'isRented', 'isDelayed');
+            $cardToChange.classList.add(carsArray[index].status);
             
-            $cardToChange.closest('li').outerHTML = cardHtml;
+            $statusIcon[0].innerHTML = setStatusIcon(index);
+            $statusIcon[1].innerHTML = setStatusIcon(index);
+            $statusText.innerHTML = setStatusTitle(index);
+            $switcherValue.setAttribute("value", setLittleCarPosition(index));
         }
-        
+                
         function setFlipButtons(carIndex){
             
             var $thisCard = document.querySelector('[array-count="' + carIndex + '"]');
-            
             var $cardFlipButtons = $thisCard.querySelectorAll('[data-js="flip-btn"]');
             
             for (var x = 0; x < $cardFlipButtons.length; x++){
                $cardFlipButtons[x].addEventListener('click', function(){
-                   event.preventDefault();
                    $thisCard.classList.toggle('is-switched');
                });
             }
@@ -166,7 +167,6 @@
             var $statusSwitcher = new DOM('[data-js="status-switcher"]');
             $statusSwitcher.on('click', changeCarStatus);                        
         }
-        
     }
 
     window.app = app;
